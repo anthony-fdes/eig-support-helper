aac77789ce66e2754ed9e54fc361daac1fc25b23
Anthony Fernandes <afdes25@gmail.com>
2020-03-22 15:49:02 +0530

    Additional Context Menu for WhatsMyDns added

--- background.js
+++ background.js
@@ -25,6 +25,46 @@ function openMXToolBox(info, tab) {
   });
 }
 
+function openWhatsMyDnsARecord(info, tab) {
+  selectedText = info.selectionText;
+    
+  chrome.tabs.create({
+    url: "https://www.whatsmydns.net/#A/" + selectedText
+  });
+}
+
+function openWhatsMyDnsNSRecord(info, tab) {
+  selectedText = info.selectionText;
+    
+  chrome.tabs.create({
+    url: "https://www.whatsmydns.net/#NS/" + selectedText
+  });
+}
+
+function openWhatsMyDnsMXRecord(info, tab) {
+  selectedText = info.selectionText;
+    
+  chrome.tabs.create({
+    url: "https://www.whatsmydns.net/#MX/" + selectedText
+  });
+}
+
+function openWhatsMyDnsTXTRecord(info, tab) {
+  selectedText = info.selectionText;
+    
+  chrome.tabs.create({
+    url: "https://www.whatsmydns.net/#TXT/" + selectedText
+  });
+}
+
+function openWhatsMyDnsCNAMERecord(info, tab) {
+  selectedText = info.selectionText;
+    
+  chrome.tabs.create({
+    url: "https://www.whatsmydns.net/#CNAME/" + selectedText
+  });
+}
+
 /**
 * Register All Menu Items
 */
@@ -44,4 +84,45 @@ var contextMenuMXToolBox = chrome.contextMenus.create({
   "title": "Check MX Tool Box Details",
   "contexts":["selection"],
   "onclick": openMXToolBox
+});
+
+var contextMenuWhatsMyDns = chrome.contextMenus.create({
+  "title": "Check Whats My DNS Details",
+  "id" : "parent",
+  "contexts":["selection"]
+});
+
+var contextMenuARecord = chrome.contextMenus.create({
+  "title": "Fetch the A Record",
+  "parentId" : "parent",
+  "contexts":["selection"],
+  "onclick": openWhatsMyDnsARecord
+});
+
+var contextMenuARecord = chrome.contextMenus.create({
+  "title": "Fetch the NS Record",
+  "parentId" : "parent",
+  "contexts":["selection"],
+  "onclick": openWhatsMyDnsNSRecord
+});
+
+var contextMenuARecord = chrome.contextMenus.create({
+  "title": "Fetch the MX Record",
+  "parentId" : "parent",
+  "contexts":["selection"],
+  "onclick": openWhatsMyDnsMXRecord
+});
+
+var contextMenuARecord = chrome.contextMenus.create({
+  "title": "Fetch the TXT Record",
+  "parentId" : "parent",
+  "contexts":["selection"],
+  "onclick": openWhatsMyDnsTXTRecord
+});
+
+var contextMenuARecord = chrome.contextMenus.create({
+  "title": "Fetch the CNAME Record",
+  "parentId" : "parent",
+  "contexts":["selection"],
+  "onclick": openWhatsMyDnsCNAMERecord
 });
